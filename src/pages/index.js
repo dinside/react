8188705716a6059eeccd8e52fb7dcbff0cf14872
dinside <73190689+dinside@users.noreply.github.com>
2020-11-11 10:32:00 +0500

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,295 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Icon, Box, Input, Button } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Override, Menu, Section, StackItem, Stack, Formspree } from "@quarkly/components";
+import { MdLocationOn, MdEmail, MdPhone } from "react-icons/md";
+import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section>
+			<Text margin="0px 0 24px 0" text-align="center" font="--headline2" md-font="--headline3">
+				DEAD INSIDE
+			</Text>
+			<Menu
+				display="flex"
+				justify-content="center"
+				font="--base"
+				font-weight="700"
+				md-flex-direction="column"
+				md-align-items="center"
+			>
+				<Override slot="link" text-decoration="none" color="--dark" padding="6px 12px" />
+				<Override slot="link-active" color="--primary" />
+				<Override slot="item" padding="6px" />
+				<Override slot="link-404">
+					about
+				</Override>
+				<Override slot="link-index">
+					news
+				</Override>
+			</Menu>
+		</Section>
+		<Section background="--color-darkL2" padding="64px 0" sm-padding="40px 0">
+			<Stack>
+				<StackItem width="50%" lg-width="100%">
+					<Override
+						slot="StackItemContent"
+						margin-right="-20%"
+						padding-bottom="120%"
+						background="url(https://uploads.quarkly.io/5fab6b49b1c8af001eb50437/images/RB9lEF.png?v=2020-11-11T05:01:04.836Z) 50% 15%/cover"
+						lg-margin-right="0"
+						lg-padding-bottom="80%"
+					/>
+				</StackItem>
+				<StackItem width="50%" lg-width="100%">
+					<Override
+						slot="StackItemContent"
+						padding="98px 64px"
+						margin-left="-20%"
+						margin-top="36px"
+						background="--color-red"
+						color="--light"
+						mix-blend-mode="lighten"
+						lg-margin-left="0"
+						lg-margin-top="0"
+						flex-direction="column"
+					/>
+					<Text
+						as="h4"
+						margin="0"
+						font="--base"
+						letter-spacing="1px"
+						text-transform="uppercase"
+					>
+						About me
+					</Text>
+					<Text as="h1" margin="0 0 16px 0" font="--headline1" lg-font="--headline2">
+						Hey I’m ghoul
+					</Text>
+					<Text as="p" margin="16px 0" font="--base" max-width="400px">
+						<br />
+						<br />
+						Gul - a mythical creature and folklore character, a werewolf in Arabian, Persian and Turkic mythology. Usually depicted as a creature with a hideous appearance and donkey hooves that do not disappear during any transformations.{"\n\n"}
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section
+			color="--light"
+			padding="100px 0"
+			sm-padding="40px 0"
+			position="relative"
+			background="linear-gradient(0deg,rgba(25, 30, 34, 0.8) 0%,rgba(0,0,0,0) 21%,rgba(25, 30, 34, 0.8) 100%),--color-darkL2 url(https://images.unsplash.com/photo-1547619292-240402b5ae5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80)"
+		>
+			<Stack>
+				<StackItem width="50%" md-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box
+						padding="0 0 0 64px"
+						sm-padding="64px 0 0 0"
+						margin="32px 0 0 0"
+						max-width="360px"
+						position="relative"
+					>
+						<Icon
+							position="absolute"
+							size="48px"
+							top="0"
+							left="0"
+							category="md"
+							icon={MdLocationOn}
+						/>
+						<Text as="h4" margin="6px 0" font="--base">
+							Visit us
+						</Text>
+						<Text as="p" margin="6px 0" font="--headline3">
+							4998 Hanover Street, New York, 10011
+						</Text>
+					</Box>
+					<Box
+						padding="0 0 0 64px"
+						sm-padding="64px 0 0 0"
+						margin="64px 0 0 0"
+						max-width="360px"
+						position="relative"
+					>
+						<Icon
+							position="absolute"
+							size="48px"
+							top="0"
+							left="0"
+							category="md"
+							icon={MdEmail}
+						/>
+						<Text as="h4" margin="6px 0" font="--base">
+							Email us
+						</Text>
+						<Text as="p" margin="6px 0" font="--headline3">
+							<Link href="mailto:hello@company.com" text-decoration="none" hover-text-decoration="underline" color="--light">
+								esteymarshmallow@gmail.com
+							</Link>
+						</Text>
+					</Box>
+					<Box padding="0 0 0 64px" margin="64px 0 0 0" max-width="360px" position="relative">
+						<Icon
+							position="absolute"
+							size="48px"
+							top="0"
+							left="0"
+							category="md"
+							icon={MdPhone}
+						/>
+						<Text as="h4" margin="6px 0" font="--base">
+							Call us
+						</Text>
+						<Text as="p" margin="6px 0" font="--headline3">
+							+1 (234) 666-66-66            +1 (234) 666-66-66
+						</Text>
+					</Box>
+					<Box
+						padding="0 0 0 64px"
+						sm-padding="0"
+						margin="48px 0"
+						max-width="360px"
+						position="relative"
+						display="flex"
+					>
+						<Icon
+							category="fa"
+							icon={FaFacebookF}
+							width="48px"
+							height="48px"
+							size="24px"
+							margin-right="16px"
+							background="--color-primary"
+							border-radius="50%"
+						/>
+						<Icon
+							category="fa"
+							icon={FaTwitter}
+							width="48px"
+							height="48px"
+							size="24px"
+							margin-right="16px"
+							background="--color-primary"
+							border-radius="50%"
+						/>
+						<Icon
+							category="fa"
+							icon={FaLinkedinIn}
+							width="48px"
+							height="48px"
+							size="24px"
+							margin-right="16px"
+							background="--color-primary"
+							border-radius="50%"
+						/>
+					</Box>
+				</StackItem>
+				<StackItem width="50%" md-width="100%">
+					<Box
+						max-width="360px"
+						padding="56px 48px"
+						margin="0 0 0 auto"
+						md-max-width="100%"
+						background="--color-indigo"
+					>
+						<Text as="h3" font="--headline3" margin="0 0 20px 0">
+							Leave us message
+						</Text>
+						<Formspree endpoint="DONE" completeText="12">
+							<Stack gap="16px">
+								<StackItem width="100%">
+									<Override slot="StackItemContent" flex-direction="column" />
+									<Text font="--base" margin="0 0 4px 0">
+										Name
+									</Text>
+									<Input width="100%" name="name" />
+								</StackItem>
+								<StackItem width="100%">
+									<Override slot="StackItemContent" flex-direction="column" />
+									<Text font="--base" margin="0 0 4px 0">
+										Email
+									</Text>
+									<Input width="100%" type="email" name="email" />
+								</StackItem>
+								<StackItem width="100%">
+									<Override slot="StackItemContent" flex-direction="column" />
+									<Text font="--base" margin="0 0 4px 0">
+										Message
+									</Text>
+									<Input as="textarea" rows="4" width="100%" name="message" />
+								</StackItem>
+								<StackItem width="100%">
+									<Button background="--color-dark">
+										Send
+									</Button>
+								</StackItem>
+							</Stack>
+						</Formspree>
+					</Box>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section background-color="--dark" text-align="center" padding="32px 0">
+			<Menu
+				display="flex"
+				justify-content="center"
+				font="--lead"
+				font-weight="700"
+				margin="-6px 0 16px"
+				md-flex-direction="column"
+				md-align-items="center"
+			>
+				<Override slot="link" text-decoration="none" color="--light" padding="6px 12px" />
+				<Override slot="link-active" color="--primary" />
+				<Override slot="item" padding="6px 0px" />
+				<Override slot="link-404">
+					about
+				</Override>
+				<Override slot="link-index">
+					news
+				</Override>
+			</Menu>
+			<Link
+				href="mailto:hello@company.com"
+				text-decoration-line="none"
+				variant="--base"
+				color="--grey"
+				hover-color="--primary"
+			>
+				esteymarshmallow@company.com
+			</Link>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fab6b49b1c8af001eb5043a",
+				"5fab6b49b1c8af001eb5043c"
+			]
+		},
+		"5fab6b49b1c8af001eb5043a": {
+			"id": "5fab6b49b1c8af001eb5043a",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fab6b49b1c8af001eb5043c": {
+			"id": "5fab6b49b1c8af001eb5043c",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
